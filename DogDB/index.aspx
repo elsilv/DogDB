e630d2d93799b470680130a210a108fbeccf82c6
elsilv <elsi.silvennoinen@gmail.com>
2021-05-03 14:05:57 +0300

    Add project files.

---
+++ DogDB/index.aspx
@@ -0,0 +1,83 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="DogDB.index" %>
+
+<!DOCTYPE html>
+<script runat="server">
+</script>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <center>
+                <h1>Dog Database</h1>
+                <asp:Label ID="Label1" runat="server" TextColor="#33CC33"></asp:Label>
+                <hr />
+
+                 <div> 
+                 <h2>Add new Dog</h2>
+            <table class="auto-style1">  
+                <tr>  
+                    <td>Name :</td>  
+                    <td>  
+                        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>  
+                    </td>  
+  
+               </tr>  
+                <tr>  
+                    <td>Birthday</td>  
+                     <td> <asp:TextBox ID="TextBox3" runat="server" TextMode="Date"></asp:TextBox></td>  
+                </tr>  
+                <tr>  
+                    <td>Gender</td>  
+                    <td>  
+                        <asp:RadioButtonList ID="RadioButtonList1" runat="server">  
+                            <asp:ListItem>Male</asp:ListItem>  
+                            <asp:ListItem>Female</asp:ListItem>  
+                        </asp:RadioButtonList>  
+                    </td>  
+               </tr>  
+                <tr>  
+                    <td>Owner id</td>  
+                    <td>  
+                        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>  
+                    </td>  
+                </tr>  
+                <tr>  
+                    <td>  
+                        <input id="Submit2" type="submit" value="submit"/> 
+                    </td>  
+                </tr>  
+            </table>  
+        </div>  
+
+                <p> Search by </p>
+
+                  <asp:DropDownList ID="DropDowntList1" runat="server">
+                    <asp:ListItem Selected="True" Value="name"> Dog name </asp:ListItem>
+                    <asp:ListItem Value="gender"> Dog gender </asp:ListItem>
+                    <asp:ListItem Value="dog_id"> Dog id </asp:ListItem>
+                    <asp:ListItem Value="owner_id"> Dog by Owner id </asp:ListItem>
+                    <asp:ListItem Value="user_id"> User by Owner id </asp:ListItem>
+                      <asp:ListItem Value="username"> User by name </asp:ListItem>
+                  </asp:DropDownList>
+
+                <asp:TextBox ID="Text1search" runat="server"></asp:TextBox>
+                <input id="Submit1" type="submit" value="submit"/>
+
+                 <p> Delete dog using id</p>
+                <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+                <input id="Submit3" type="submit" value="delete"/>
+                <hr />
+
+                <br />
+
+                <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+
+            </center>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DogDB/index.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Npgsql;
+using System.Data;
+
+namespace DogDB
+{
+	public partial class index : System.Web.UI.Page
+	{
+		private DataSet ds = new DataSet();
+		private DataTable dt = new DataTable();
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+			try
+			{
+				Label1.Text = "Connection success";
+
+				NpgsqlConnection conn = new NpgsqlConnection("Server=192.168.1.107;Port=5432;Database=tjta3501;User Id=elsi;Password=sitäkissaostaisi;");
+				conn.Open();
+
+				NpgsqlCommand comm = new NpgsqlCommand();
+				comm.Connection = conn;
+				comm.CommandType = CommandType.Text;
+				comm.CommandText = "SELECT * FROM public.\"Dog\" ORDER BY bday DESC";
+
+
+				if (Text1search.Text.Length >= 2)
+                {
+					if (DropDowntList1.SelectedItem.Value == "name")
+					{
+						comm.CommandText = "SELECT * FROM public.\"Dog\" WHERE name like'%" + Text1search.Text + "%' ORDER BY name ASC";
+					}
+
+					if (DropDowntList1.SelectedItem.Value == "dog_id")
+					{
+						comm.CommandText = "SELECT * FROM public.\"Dog\" WHERE dog_id like'%" + Text1search.Text + "%' ORDER BY bday DESC";
+					}
+
+					if (DropDowntList1.SelectedItem.Value == "gender")
+					{
+						comm.CommandText = "SELECT * FROM public.\"Dog\" WHERE gender like'%" + Text1search.Text + "%' ORDER BY bday DESC";
+					}
+
+					if (DropDowntList1.SelectedItem.Value == "owner_id")
+					{
+						comm.CommandText = "SELECT * FROM public.\"Dog\" WHERE owner_id ::text like'%" + Text1search.Text + "%' ORDER BY owner_id ASC";
+					}
+
+					if (DropDowntList1.SelectedItem.Value == "user_id")
+					{
+						comm.CommandText = "SELECT * FROM public.\"User\" WHERE owner_id ::text like'%" + Text1search.Text + "%' ORDER BY owner_id ASC";
+					}
+
+					if (DropDowntList1.SelectedItem.Value == "username")
+					{
+						comm.CommandText = "SELECT * FROM public.\"User\" WHERE name like'%" + Text1search.Text + "%' ORDER BY name ASC";
+					}
+				}
+
+				if (TextBox5.Text.Length >= 6)
+				{
+					NpgsqlCommand del = new NpgsqlCommand("DELETE FROM public.\"Dog\" WHERE dog_id like'%" + TextBox5.Text + "%'", conn);
+					del.ExecuteNonQuery();
+
+					TextBox5.Text = string.Empty;
+				}
+
+				if (TextBox1.Text.Length > 3 & TextBox3.Text.Length > 6 & TextBox4.Text.Length > 0) 
+				{
+					long id = 10000000000;
+					Random random = new Random();
+					int num = random.Next(100000000, 999999999);
+					id += num;
+					
+					NpgsqlCommand cmd = new NpgsqlCommand("insert into public.\"Dog\" values('"+ id + "', '" + TextBox1.Text + "', '" + RadioButtonList1.SelectedValue + "', '" + TextBox3.Text + "', '" + TextBox4.Text + "')", conn);
+					cmd.ExecuteNonQuery();
+
+					TextBox1.Text = string.Empty;
+					TextBox3.Text = string.Empty;
+					TextBox4.Text = string.Empty;
+				}
+
+				NpgsqlDataAdapter da = new NpgsqlDataAdapter(comm);
+
+				da.Fill(dt);
+				comm.Dispose();
+				conn.Close();
+
+				GridView1.DataSource = dt;
+				GridView1.DataBind();
+				
+			}
+			catch (Exception)
+			{
+				Label1.Text = "Something gone wrong...";
+				throw;
+			}
+		}
+	}
+}
